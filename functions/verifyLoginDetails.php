9e9f30d4089084aa659fb051544f23ecc35d501e
Alexandra <apappas@student.wethinkcode.co.za>
2019-11-07 18:41:18 +0200

    login access restricted

--- functions/verifyLoginDetails.php
+++ functions/verifyLoginDetails.php
@@ -21,11 +21,17 @@ function email_verification($user, $email){
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $_SESSION['error'] = NULL;
         //Checks whether username already exists.
-        $stmt = $dbh->prepare("SELECT * FROM `user` WHERE (`username`=? AND `password`=?);");
+        $stmt = $dbh->prepare("SELECT * FROM `user` WHERE (`username`=? AND `password`=?  AND `verified`=1);");
         $stmt->execute([$username, $password]);
         if ($stmt->rowCount() == 1) {
             $stmt = NULL;
             return (1);
+        }
+        $usr = $dbh->prepare("SELECT * FROM `user` WHERE (`username`=? AND `password`=?);");
+        $usr->execute([$username, $password]);
+        if ($stmt->rowCount() == 0 && $usr->rowCount() == 1) {
+            $_SESSION['error'] = "Please validate your email and try again";
+            return (0);
         }
         else {
             $_SESSION['error'] = "Username or password is incorrect.";
