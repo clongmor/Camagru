641c3bb64629849b1aae40819103af976499ec6f
Chloe Longmore <clongmor@student.wethinkcode.co.za>
2019-11-10 21:48:03 +0200

    updated editor page to have a form

---
+++ user_images.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+	include "templates/header.php";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Madimgz - My Images</title>
+	<style>
+		.center {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	</style>
+</head>
+
+<body class="purp_body">
+	<section class="hero is-fullheight">
+		<div class="hero-body">
+			<div class="container">
+				<figure class="image center">
+					
+				</figure>
+			</div>
+		</div>
+	</section>
+	<?php
+	include "templates/footer.php";
+?>
+</body>
+
+</html>
