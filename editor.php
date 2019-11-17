f09cf7aee667deedd1a67596c8038101550cbe69
Alexandra <apappas@student.wethinkcode.co.za>
2019-11-17 16:30:43 +0200

    moved jsFunctions

--- editor.php
+++ editor.php
@@ -9,6 +9,7 @@
 <html>
 
 <head>
+	<script src = "functions/jsFunctions/editorFunctions.js"> </script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Madimgz - Editor</title>
