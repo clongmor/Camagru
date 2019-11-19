ca9b51b68edbc5ca3115f35c0b9d3ab060b6a3c0
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2019-11-19 16:47:42 +0200

    added copy of database settings to database.php

--- config/database.php
+++ config/database.php
@@ -5,3 +5,13 @@
     $DB_USER = "root";
     $DB_PASSWORD = "12345678";
 ?>
+
+
+    <!--
+    Wouter's database settings:
+        $DB_NAME = "camagru";
+        $DB_DSN = "mysql:host=127.0.0.1;dbname=".$DB_NAME;
+        $DB_DSN_LIGHT = "mysql:host=127.0.0.1";
+        $DB_USER = "root";
+        $DB_PASSWORD = "12345678";
+    -->
