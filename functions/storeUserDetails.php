978d024ae1915ab4c3ce78faf6c68ad5c387c3ee
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2019-11-19 15:49:33 +0200

    added default image to newly signup user

--- functions/storeUserDetails.php
+++ functions/storeUserDetails.php
@@ -33,8 +33,8 @@
         //Inserts details if username and email does not exist.
         $_SESSION['token'] = md5( rand(0,1000) );
         $token = $_SESSION['token'];
-        $stmt = $dbh->prepare("INSERT INTO `user` (`username`, `password`, `email`, `token`) VALUES (?, ?, ?, ?);");
-        if ($stmt->execute([$username, hash('whirlpool', $password), $email, $token])) {
+        $stmt = $dbh->prepare("INSERT INTO `user` (`username`, `password`, `email`, `token`, `picture`) VALUES (?, ?, ?, ?);");
+        if ($stmt->execute([$username, hash('whirlpool', $password), $email, $token, "defaultprofile.jpg"])) {
             $stmt = null;
             return (1);
         } else {
