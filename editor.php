6510e6f4b017c6a02d485a67b79493966c20e9b4
Chloe Longmore <clongmor@student.wethinkcode.co.za>
2019-11-19 14:56:53 +0200

    fixed errors on editor page

--- editor.php
+++ editor.php
@@ -62,7 +62,6 @@ include "templates/header.php";
 				Upload an image from your computer or take a picture with your webcam.
 				PRO TIP: make sure you click submit if uploading from your computer!
               </h1>
-			  <?php if (isset($_SESSION['username'])): ?> 
                 <form action="functions/galleryFunctions.php" method="post" enctype="multipart/form-data">
                 <input type="hidden" name="action" value="uploadUserImage">
 				  <div class="field">
@@ -207,7 +206,6 @@ include "templates/header.php";
 		</div>
 	</div>
   </section>
-<<<<<<< HEAD
 <?php endif; ?>
 
 <?php if (!isset($_SESSION['username'])): ?>
@@ -225,26 +223,6 @@ include "templates/header.php";
 <?php
 include "templates/footer.php";
 ?>
-=======
-  </body>
-<?php endif; ?>
-
-<?php if (!isset($_SESSION['username'])) : ?>
-<body class="purp_body">
-	<section class="hero is-fullheight">
-		<div class="hero-body">
-			<div class="container">
-				<h1 class="title center">
-					Hi, you don't seem to have access to this page. Please login and then try again.
-				</h1>
-			</div>
-		</div>
-	</section>
->>>>>>> f4656f7b866546cfa70043a710338385ed526c04
 </body>
-<?php endif; ?>
-	<?php
-	include "templates/footer.php";
-	?>
 
 </html>
