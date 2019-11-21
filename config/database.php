fce78039923f76a2265015d4622516da3b4ed679
Chloe Longmore <clongmor@student.wethinkcode.co.za>
2019-11-21 15:22:50 +0200

    dunno

--- config/database.php
+++ config/database.php
@@ -1,9 +1,17 @@
 <?php
+    // $DB_NAME = "camagru";
+    // $DB_DSN = "mysql:host=127.0.0.1;dbname=".$DB_NAME;
+    // $DB_DSN_LIGHT = "mysql:host=127.0.0.1";
+    // $DB_USER = "root";
+    // $DB_PASSWORD = "12345678";
+
+    // Chloe's database settings
     $DB_NAME = "camagru";
-    $DB_DSN = "mysql:host=127.0.0.1;dbname=".$DB_NAME;
-    $DB_DSN_LIGHT = "mysql:host=127.0.0.1";
+    $DB_DSN = "mysql:host=127.0.0.1:3307;dbname=".$DB_NAME;
+    $DB_DSN_LIGHT = "mysql:host=127.0.0.1:3307";
     $DB_USER = "root";
     $DB_PASSWORD = "12345678";
+
     // <!--
     // Wouter's database settings:
     // $DB_NAME = "camagru";
